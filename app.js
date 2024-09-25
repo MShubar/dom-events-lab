532803b38c641f59769ef17668e1aaff5dc1d3b2
MShubar <pokeeeeepow@gmail.com>
2024-09-25 10:36:14 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+/*-------------------------------- Constants --------------------------------*/
+const numberButton = document.querySelector('id')
+const operatorButton = document.querySelector('button operator')
+const inputElement = document.querySelector('input')
+const display = document.querySelector('p')
+const displayElement = numberButton.innerHTML
+/*-------------------------------- Variables --------------------------------*/
+
+console.dir(numberButton)
+
+for (i = 0; i <= 9; i++) {
+  displayElement.numberButton
+}
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+numberButton.addEventListener('click', displayNumber)
+
+/*-------------------------------- Functions --------------------------------*/
