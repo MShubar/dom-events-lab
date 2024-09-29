cffd7cfc3dec446460d0de660b7fa179459938f0
MShubar <pokeeeeepow@gmail.com>
2024-09-29 08:40:16 +0300

    last commit

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
 /*-------------------------------- Constants --------------------------------*/
-const buttons = document.querySelectorAll('.button')
-const display = document.querySelector('.display p')
+const buttons = document.querySelectorAll(".button")
+const display = document.querySelector(".display p")
 
 /*-------------------------------- Variables --------------------------------*/
-let currentValue = ''
-let operator = ''
-let firstValue = null
+let currentValue = ""
+let operator = ""
+let firstValue = ""
 let isResultDisplayed = false
 
 /*----------------------------- Event Listeners -----------------------------*/
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     const buttonValue = button.textContent
-    if (button.classList.contains('number')) {
+    if (button.classList.contains("number")) {
       if (isResultDisplayed) {
-        currentValue = ''
+        currentValue = ""
         isResultDisplayed = false
       }
       currentValue += buttonValue
       display.textContent = currentValue
-    } else if (button.classList.contains('operator')) {
-      if (buttonValue === 'C') {
-        currentValue = ''
-        firstValue = null
-        operator = ''
-        display.textContent = '0'
+    } else if (button.classList.contains("operator")) {
+      if (buttonValue === "C") {
+        currentValue = ""
+        firstValue = ""
+        operator = ""
+        display.textContent = "0"
       } else if (currentValue && !firstValue) {
         firstValue = parseInt(currentValue)
         operator = buttonValue
-        currentValue = ''
+        currentValue = ""
       } else if (firstValue && currentValue && operator) {
         firstValue = calculate(firstValue, parseInt(currentValue), operator)
         operator = buttonValue
         display.textContent = firstValue
-        currentValue = ''
+        currentValue = ""
       }
-    } else if (button.classList.contains('equals')) {
+    } else if (button.classList.contains("equals")) {
       if (firstValue !== null && currentValue && operator) {
         const result = calculate(firstValue, parseInt(currentValue), operator)
         display.textContent = result
-        firstValue = null
-        operator = ''
-        currentValue = ''
+        firstValue = ""
+        operator = ""
+        currentValue = ""
         isResultDisplayed = true
       }
     }
@@ -51,14 +51,13 @@ buttons.forEach((button) => {
 /*-------------------------------- Functions --------------------------------*/
 
 function calculate(num1, num2, operator) {
-  switch (operator) {
-    case '+':
-      return num1 + num2
-    case '-':
-      return num1 - num2
-    case '*':
-      return num1 * num2
-    case '/':
-      return num1 / num2
+  if (operator === "+") {
+    return num1 + num2
+  } else if (operator === "-") {
+    return num1 - num2
+  } else if (operator === "*") {
+    return num1 * num2
+  } else if (operator === "/") {
+    return num1 / num2
   }
 }
