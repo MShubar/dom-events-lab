04ed91622008001c259f0511e21795cf22c736fa
MShubar <pokeeeeepow@gmail.com>
2024-09-29 15:35:04 +0300

    last commit

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 /*-------------------------------- Constants --------------------------------*/
+//Assign the buttons to a variables
 const buttons = document.querySelectorAll(".button")
+//Assign the display to a variables
 const display = document.querySelector(".display p")
 
 /*-------------------------------- Variables --------------------------------*/
+// value of what number I clicked
 let currentValue = ""
+// value of what operator I clicked
 let operator = ""
+// what is stored from the current value is changed to first value
 let firstValue = ""
+// is my display showing the results.. well I didn't do the operation yet to false?
 let isResultDisplayed = false
 
 /*----------------------------- Event Listeners -----------------------------*/
+// just listens to each click and I will decide weather I want functions inside other functions or 1 large function
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const buttonValue = button.textContent
+    //simply the number should always be the current value if clicked unless result is displayed
     if (button.classList.contains("number")) {
       if (isResultDisplayed) {
         currentValue = ""
@@ -19,23 +27,32 @@ buttons.forEach((button) => {
       }
       currentValue += buttonValue
       display.textContent = currentValue
-    } else if (button.classList.contains("operator")) {
+    }
+    // operators usage
+    else if (button.classList.contains("operator")) {
+      // C changes display to 0
       if (buttonValue === "C") {
         currentValue = ""
         firstValue = ""
         operator = ""
         display.textContent = "0"
-      } else if (currentValue && !firstValue) {
+      }
+      // Just assigns current value to first value of course needs to be changed to integer
+      else if (currentValue && !firstValue) {
         firstValue = parseInt(currentValue)
         operator = buttonValue
         currentValue = ""
-      } else if (firstValue && currentValue && operator) {
+      }
+      // I have everything so just calculate
+      else if (firstValue && currentValue && operator) {
         firstValue = calculate(firstValue, parseInt(currentValue), operator)
         operator = buttonValue
         display.textContent = firstValue
         currentValue = ""
       }
-    } else if (button.classList.contains("equals")) {
+    }
+    // equals alone sad
+    else if (button.classList.contains("equals")) {
       if (firstValue !== null && currentValue && operator) {
         const result = calculate(firstValue, parseInt(currentValue), operator)
         display.textContent = result
@@ -50,7 +67,8 @@ buttons.forEach((button) => {
 
 /*-------------------------------- Functions --------------------------------*/
 
-function calculate(num1, num2, operator) {
+// operations requires num 1 num 2 and an operator
+const calculate = (num1, num2, operator) => {
   if (operator === "+") {
     return num1 + num2
   } else if (operator === "-") {
